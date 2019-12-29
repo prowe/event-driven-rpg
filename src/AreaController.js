09672c5d87c8098f41d6ca3e41fc5eaf569d964d
Paul Rowe <prowe@sourceallies.com>
2019-12-28 22:06:50 -0600

    Getting tricky

--- src/AreaController.js
+++ src/AreaController.js
@@ -1,11 +1,20 @@
-import React, { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useEventBus } from './EventContext';
 import areas from './areas';
 import { filter } from 'rxjs/operators';
 
+function buildActorsByAreaMap() {
+    return areas.reduce((actorsByArea, area) => {
+        return {
+            ...actorsByArea,
+            [area.id]: new Map()
+        }
+    }, {});
+}
 
 export default function AreaController() {
     const {subject, broadcastEvent} = useEventBus();
+    const actorsByArea = useRef(buildActorsByAreaMap());
 
     useEffect(() => {
         function isEnterAreaEvent(event) {
@@ -13,13 +22,18 @@ export default function AreaController() {
         }
 
         function next({areaId, actor}) {
+            const areaActors = actorsByArea.current[areaId];
+            areaActors.set(actor.id, actor);
+            console.log('area actors', areaId, areaActors);
+
             const area = areas.find(({id}) => id === areaId);
             if (area) {
                 broadcastEvent({
                     name: 'observed-area',
                     actor,
                     areaId,
-                    area
+                    area,
+                    nearby: [...areaActors.values()]
                 });
             }
         }
@@ -29,6 +43,22 @@ export default function AreaController() {
             .subscribe({next});
         return () => subscription.unsubscribe();
     }, [subject, broadcastEvent]);
+
+    useEffect(() => {
+        function isExitedAreaEvent(event) {
+            return event.name === 'exited-area';
+        }
+
+        function next({areaId, actor}) {
+            const areaActors = actorsByArea.current[areaId];
+            areaActors.delete(actor.id);
+        }
+
+        const subscription = subject
+            .pipe(filter(isExitedAreaEvent))
+            .subscribe({next});
+        return () => subscription.unsubscribe();
+    }, [subject]);
 
     return null;
 }
--- src/Nearby.js
+++ src/Nearby.js
@@ -5,12 +5,29 @@ import {filter} from 'rxjs/operators';
 
 export default function Nearby({player}) {
     function nearbyEventReducer(currentState, event) {
+        // FIXME: lot of odd race conditions
         const {actor, areaId, name} = event;
         if (actor.id === player.id) {
-            if (currentState.currentAreaId !== event.areaId) {
+            // if (currentState.currentAreaId !== event.areaId
+            //     && name === 'entered-area') {
+            //     return {
+            //         areaId,
+            //         nearby: {
+            //             [actor.id]: actor
+            //         }
+            //     };
+            // }
+            if (name === 'observed-area') {
+                const nearby = event.nearby.reduce((nearby, actor) => {
+                    return {
+                        ...nearby,
+                        [actor.id]: actor
+                    };
+                }, {});
+                console.log(player.id, 'observed: ', nearby)
                 return {
                     areaId,
-                    nearby: {}
+                    nearby
                 };
             }
         } else {
@@ -43,7 +60,7 @@ export default function Nearby({player}) {
 
     useEffect(() => {
         function isReleventEvent(event) {
-            const eventNames = ['entered-area', 'exited-area'];
+            const eventNames = ['entered-area', 'exited-area', 'observed-area'];
             return eventNames.includes(event.name);
         }
 
